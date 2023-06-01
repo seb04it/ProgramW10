87047c146e081192de1269ad834ff1abb5dcbb75
seb04it <seb04it@gmail.com>
2023-06-01 16:54:29 +0200

    ProgramW10

---
+++ ProgramW10/ProgramW10/Employee.cs
@@ -0,0 +1,60 @@
+﻿namespace ProgramW10
+{
+    public class Employee
+    {
+        private List<float> grades = new List<float>();
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = name;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public void AddGrade(float grade)
+        {
+            if(grade >=0 && grade <=100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid grade?");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if(float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("Wrong type of string?");
+            }
+        }
+        public void AddGrade(double grade)
+        {
+            
+        }
+
+        public Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+            statistics.Average = 0;
+
+            foreach(var grade in this.grades)
+            {
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
+            }
+            statistics.Average /= this.grades.Count;
+            return statistics;
+        }
+    }
+}
---
+++ ProgramW10/ProgramW10/Program.cs
@@ -0,0 +1,13 @@
+﻿using ProgramW10;
+
+var employee = new Employee("K", "L");
+employee.AddGrade("101");
+employee.AddGrade("Adam");
+employee.AddGrade(2);
+employee.AddGrade(6);
+employee.AddGrade(2000);
+var statistics = employee.GetStatistics();
+
+Console.WriteLine($"max: {statistics.Max}");
+Console.WriteLine($"min: {statistics.Min}");
+Console.WriteLine($"average: {statistics.Average:n2}");
